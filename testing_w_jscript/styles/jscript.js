a84d9bcc2d2cfc3b491fac0f7da8e3680813c361
Conor O'Brien <91850829+conorep@users.noreply.github.com>
2021-10-05 08:53:21 -0700

    Add files via upload
    added more buttons, edited to only allow one open area at a time
    
    (redundant code needs fixing)

--- testing_w_jscript/styles/jscript.js
+++ testing_w_jscript/styles/jscript.js
@@ -1,17 +1,51 @@
+
+// get all of instance of theDropStuff
+let dropdowns = document.getElementsByClassName("theDropStuff");
+
 function dropFunction() {
+    realDropFunction();
     document.getElementById("dropID").classList.toggle("show");
+
+}
+
+function dropFunction1() {
+    realDropFunction();
+    document.getElementById("dropID1").classList.toggle("show");
+}
+
+
+function dropFunction2() {
+    realDropFunction();
+    document.getElementById("dropID2").classList.toggle("show");
+}
+
+function dropFunction3() {
+    realDropFunction();
+    document.getElementById("dropID3").classList.toggle("show");
+}
+
+function realDropFunction() {
+
+    for (let i = 0; i < dropdowns.length; i++)
+    {
+        if (dropdowns[i].classList.contains('show')) {
+            dropdowns[i].classList.remove('show');
+        }
+    }
+
 }
 
 window.onclick = function(event) {
     if (!event.target.matches('.dropBtn')) {
-        // get theDropStuff
-        let dropdowns = document.getElementsByClassName("theDropStuff");
 
         // run through the one position that dropdowns will have and add or remove the show function
         for (let i = 0; i < dropdowns.length; i++) {
             let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            // if (openDropdown.classList.contains('show')) {
+            //     openDropdown.classList.remove('show');
+            // }
+            if (dropdowns[i].classList.contains('show')) {
+                dropdowns[i].classList.remove('show');
             }
         }
     }
